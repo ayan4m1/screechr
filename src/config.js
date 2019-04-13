ef60c962183fe059390d4130533cab36a78f2b5e
ayan4m1 <andrew@bulletlogic.com>
2019-04-13 14:35:21 -0400

    rename app to screechr

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import cosmiconfig from 'cosmiconfig';
 
-const configSearch = cosmiconfig('discoptero').searchSync();
+const configSearch = cosmiconfig('screechr').searchSync();
 
 if (configSearch === null) {
   throw new Error(
-    'Did not find a config file for module name "discoptero" - see https://github.com/davidtheclark/cosmiconfig#explorersearch'
+    'Did not find a config file for module name "screechr" - see https://github.com/davidtheclark/cosmiconfig#explorersearch'
   );
 }
 
